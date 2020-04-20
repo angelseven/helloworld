bf51c8c850417a20876405352f9b70073e95fccd
DJ002675 <DJ002675@CXBJ-002675-L.DJBX.COM>
2020-04-20 10:13:08 +0800

    测试修改乱码

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello World Ëæ±ã¸ÄÒ»¸Ä");
+        System.out.println("Hello World æµ‹è¯•æ”¹ä¸€æ”¹ä¹±ç ");
 	}
 
 }
